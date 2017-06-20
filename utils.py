ec53e62b9e411492ae864bf41f41d9404c832192
Samuel Miravet Verde <samuelmiver@gmail.com>
2017-06-20 21:48:28 +0200

    hola

--- utils.py
+++ utils.py
@@ -408,6 +408,18 @@ def remove_column(array, index):
     return np.delete(array, np.s_[index], axis=1)
 
 
+def errorfill(x, y, yerr, color=None, alpha_fill=0.3, ax=None, label=None):
+    ax = ax if ax is not None else plt.gca()
+    if color is None:
+        color = ax._get_lines.color_cycle.next()
+    if np.isscalar(yerr) or len(yerr) == len(y):
+        ymin = y - yerr
+        ymax = y + yerr
+    elif len(yerr) == 2:
+        ymin, ymax = yerr
+    ax.plot(x, y, color=color, label=label)
+    ax.fill_between(x, ymax, ymin, color=color, alpha=alpha_fill)
+
 ##### FOR SEQUENCES
 
 def reverse_complement(seq):
