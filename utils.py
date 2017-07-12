354441126a659c1535b61579c7077821055927c9
Samuel Miravet Verde <samuelmiver@gmail.com>
2017-07-12 18:24:34 +0200

    polyfit 2

--- utils.py
+++ utils.py
@@ -19,6 +19,21 @@ def rsquared(x, y):
     return r_value**2
 
 
+def polyfit2(x, y, degree):
+    results = {}
+    coeffs = np.polyfit(x, y, degree)
+     # Polynomial Coefficients
+    results['polynomial'] = coeffs.tolist()
+    # r-squared
+    p = np.poly1d(coeffs)
+    # fit values, and mean
+    yhat = p(x)                      # or [p(z) for z in x]
+    ybar = np.sum(y)/len(y)          # or sum(y)/len(y)
+    ssreg = np.sum((yhat-ybar)**2)   # or sum([ (yihat - ybar)**2 for yihat in yhat])
+    sstot = np.sum((y - ybar)**2)    # or sum([ (yi - ybar)**2 for yi in y])
+    results['determination'] = ssreg / sstot
+    return results
+
 def splitn_str(your_string, n):
     """ Given a string, returns a list with that string splitted each n characters """
 
